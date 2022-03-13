cd2a6c59252a89877a7b28393b2db632c010615a
0take <example@example>
2022-03-13 17:55:46 +0900

    add backend

--- src/src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Header.css";
-
-function Header() {
-    return (<></>
-  );
-}
-
-export default Header;
--- src/src/ListData.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ListData() {
-  return (<>list</>)
-}
